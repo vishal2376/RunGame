e9d3582a1497fb513b28083ce524e114fabd16f4
Vishal & Singh <vishalsingh2376@gmail.com>
2023-03-06 22:21:13 +0530

    setup multiplayer loading screen,join and create lobby + UI

---
+++ Assets/Scripts/ConnectToServer.cs
@@ -0,0 +1,21 @@
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("LobbyScreen");
+    }
+}
