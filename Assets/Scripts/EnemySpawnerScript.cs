b43f742ba9eea12e6d24b553369f80279ba4d7e8
Vishal & Singh <vishalsingh2376@gmail.com>
2023-03-09 16:44:31 +0530

    reduce the spawntime of enemy of the time

--- Assets/Scripts/EnemySpawnerScript.cs
+++ Assets/Scripts/EnemySpawnerScript.cs
@@ -7,8 +7,10 @@ public class EnemySpawnerScript : MonoBehaviour
     public GameObject enemy;
     public float spawnTime = 0.5f;
 
-    private float _timer = 0;
+    [SerializeField] private float minSpawnTime = 0.15f;
+    [SerializeField] private float reduceSpawnTimeRate = 0.005f;
 
+    private float _timer = 0;
 
     void Start()
     {
@@ -18,6 +20,11 @@ public class EnemySpawnerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (spawnTime >= minSpawnTime)
+        {
+            spawnTime -= reduceSpawnTimeRate * Time.deltaTime;
+        }
+
         if (_timer < spawnTime)
         {
             _timer += Time.deltaTime;
@@ -32,10 +39,10 @@ public class EnemySpawnerScript : MonoBehaviour
     private void SpawnEnemy()
     {
         var randomLaneEdge = (Random.Range(0, 2) * 2) - 1;
-        
+
         var currentPosition = transform.position;
-        var enemySpawnPos = new Vector3(randomLaneEdge * currentPosition.x ,currentPosition.y,currentPosition.z);
-        
+        var enemySpawnPos = new Vector3(randomLaneEdge * currentPosition.x, currentPosition.y, currentPosition.z);
+
         Instantiate(enemy, enemySpawnPos, Quaternion.identity);
     }
 }
